fc90e9110e4361393e0e731c8ffcd9bf4f767836
balva <balva@DESKTOP-6PFDB9P>
2016-04-25 09:33:30 +0200

    2º cambio

--- src/miproyectolunadepluton/MiProyectoLunaDePluton.java
+++ src/miproyectolunadepluton/MiProyectoLunaDePluton.java
@@ -5,6 +5,13 @@
  */
 package miproyectolunadepluton;
 
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 /**
  *
  * @author balva
@@ -14,9 +21,18 @@ public class MiProyectoLunaDePluton {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        int b=5;
-        System.out.print("3+2 =" + b);
-    }
-    
+    public static void main(String[] args) throws TwitterException {
+    /* ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)      
+      .setOAuthConsumerKey("BNmJ2oaezMC2qciIgkpC33kYq")
+  .setOAuthConsumerSecret("1b0xCDy2Qh5nFWeuJjdF1MDcjpVJTh4LSpjTfUKeabacirUOn4")
+  .setOAuthAccessToken("708080587068858368-FxuwrHLsox7xVSQRbwjDV14vOpSxNyN")
+  .setOAuthAccessTokenSecret("c4gBOfVZL7MwyhI7jVPVrGupnNJAiZAJ2cDE9qp48OVl1");*/
+TwitterFactory tf = new TwitterFactory();
+Twitter mitwitter = tf.getInstance();   
+for(int i =0; i<11150;i++){
+Status mistatus= mitwitter.updateStatus("Me la bufa");
+System.out.println(mistatus.getText());
+
 }
+        }}
